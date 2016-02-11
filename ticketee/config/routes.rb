377dfaa41513ba0b4a1a9094607084d2ff07fb29
Ken Mitchell <kennethhmitchell@gmail.com>
2016-02-11 14:21:17 -0500

    Uploaded files are secured through attachments controller

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    root "application#index"
+ namespace :admin do
+  root "application#index"
 
-    resources :projects, only: [:new, :create, :destroy]
-    resources :users do
-      member do
-        patch :archive
-      end
-    end
+  resources :projects, only: [:new, :create, :destroy]
+  resources :users do
+   member do
+    patch :archive
+   end
   end
+ end
 
-  devise_for :users
-  root "projects#index"
+ devise_for :users
+ root "projects#index"
 
-  resources :projects, only: [:index, :show, :edit, :update] do
-    resources :tickets
-  end
+ resources :projects, only: [:index, :show, :edit, :update] do
+   resources :tickets
+ end
+ resources :attachments, only: [:show]
 end
