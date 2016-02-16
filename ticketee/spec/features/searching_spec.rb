27e69bf2247c9c25b0f06a79aae6822a74b86e17
Ken Mitchell <kennethhmitchell@gmail.com>
2016-02-16 15:41:51 -0500

    Users can click a tag's name to get a page with all tickets for the tag

--- ticketee/spec/features/searching_spec.rb
+++ ticketee/spec/features/searching_spec.rb
@@ -6,12 +6,12 @@ RSpec.feature "Users can search for tickets matching criteria" do
  let(:open) { State.create(name: "Open", default: true) }
  let(:closed) { State.create(name: "Closed") }
  let!(:ticket_1) do
-  FactoryGirl.create(:ticket, name: "Create Projects",
+  FactoryGirl.create(:ticket, name: "Create projects",
      project: project, author: user, tag_names: "iteration_1",
      state: open)
  end
  let!(:ticket_2) do
-  FactoryGirl.create(:ticket, name: "Create Users",
+  FactoryGirl.create(:ticket, name: "Create users",
      project: project, author: user, tag_names: "iteration_2",
      state: closed)
  end
@@ -25,8 +25,8 @@ RSpec.feature "Users can search for tickets matching criteria" do
   fill_in "Search", with: "tag:iteration_1"
   click_button "Search"
   within("#tickets") do
-   expect(page).to have_link "Create Projects"
-   expect(page).to_not have_link "Create Users"
+   expect(page).to have_link "Create projects"
+   expect(page).to_not have_link "Create users"
   end
  end
 
@@ -34,8 +34,17 @@ RSpec.feature "Users can search for tickets matching criteria" do
   fill_in "Search", with: "state:Open"
   click_button "Search"
   within("#tickets") do
-   expect(page).to have_link "Create Projects"
-   expect(page).to_not have_link "Create Users"
+   expect(page).to have_link "Create projects"
+   expect(page).to_not have_link "Create users"
   end
  end
+ scenario "when clicking on a tag" do
+  click_link "Create projects"
+  click_link "iteration_1"
+  within("#tickets") do
+   expect(page).to have_link "Create projects"
+   expect(page).to_not have_link "Create users"
+  end
+ end
+
 end
